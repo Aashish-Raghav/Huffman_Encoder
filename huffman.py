2aa79c516ae485163c6d4de892b1f60709a76074
aashishraghav627@gmail.com <aashishraghav627@gmail.com>
2024-12-20 21:10:27 +0530

    Huffman Encoder: Generate code and Encode text
    Write encoded text along with header(freq of text) in output file.

--- huffman.py
+++ huffman.py
@@ -29,9 +29,8 @@ class HuffmanEncoder:
         for item in (self.freq.items()):
             Node = HuffmanTreeNode(item[0],item[1])
             heapq.heappush(heap,Node)
-        heapq.heapify(heap)
 
-        while (len(heap) < 1):
+        while (len(heap) > 1):
             leftNode = heapq.heappop(heap)
             righNode = heapq.heappop(heap)
             mergeNode = HuffmanTreeNode(None,leftNode.freq+righNode.freq)
@@ -41,6 +40,25 @@ class HuffmanEncoder:
 
         return heap[0]
 
+    def _dfs(self,node,codes,current_code):
+        if (node == None):
+            return
+        
+        if (node.char != None):
+            codes[node.char] = current_code
+            return
+
+        # print(node.char,node.freq)
+        self._dfs(node.left,codes,current_code + "0")
+        self._dfs(node.right,codes,current_code + "1")
+
+
+    def build_huffman_code(self,rootNode):
+        codes = {}
+        current_code = ""
+        self._dfs(rootNode,codes,current_code)
+        return codes
+
 
     def encode(self):
         # calculate frequency
@@ -49,4 +67,12 @@ class HuffmanEncoder:
         # build huffman tree
         rootNode = self.build_huffman_tree()
 
+        #prefix-code table
+        codes = self.build_huffman_code(rootNode)
+
+        encoded_text = str(self.freq) + '\n'
+        for char in self.text:
+            encoded_text += str(codes[char])
+
+        return encoded_text
         
--- main.py
+++ main.py
@@ -4,20 +4,27 @@ from huffman import HuffmanEncoder
 
 def main():
     parser = argparse.ArgumentParser("Huffman Encoder")
-    parser.add_argument("filename",type=str,nargs=1)
+    parser.add_argument("filename",type=str,nargs=2)
     args = parser.parse_args()
 
     try:
-        file_handle = open(args.filename[0], 'r', encoding='utf-8')
+        input_file = open(args.filename[0], 'r', encoding='utf-8')
         try:
-            encode_text = file_handle.read()
+            encode_text = input_file.read()
             huffman = HuffmanEncoder(encode_text)
-            huffman.encode()
+            encoded_text = huffman.encode()
+            try:
+                output_file = open(args.filename[1],'w',encoding="utf-8")
+                output_file.write(str(encoded_text))
+            except:
+                print("Can't open output file")
+
+
 
         except:
-            print("Can't read from file")
+            print("Unexpected error occur during encoding")
     except:
-        print("can't open file")
+        print("can't open input file")
     
 
 
